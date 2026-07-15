9d10d385428771c22c9dde7d08290f21dd9e37f3
parkjihun22 <eyeful31@naver.com>
2026-07-16 07:26:05 +0900

    feat : 수정

--- src/components/InterestPopup/InterestPopup.jsx
+++ src/components/InterestPopup/InterestPopup.jsx
@@ -28,7 +28,7 @@ const InterestPopup = ({ onClose, registration, handleInputChange }) => {
     e.preventDefault();
     setIsSubmitting(true);
     try {
-      const res = await fetch("https://formspree.io/f/mrewqjaw", {
+      const res = await fetch("https://formspree.io/f/xgogozbz", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(registration),
